c43e6887414c5f4ffe9fbc9c0063ba497cbfa79d
gulsumpartal <ummugulsumpartal@gmail.com>
2017-07-27 14:21:42 +0300

    Initializes database.

--- MvcSample/MvcSample.Core/MvcSampleContext.cs
+++ MvcSample/MvcSample.Core/MvcSampleContext.cs
@@ -1,13 +1,24 @@
 ﻿using System.Configuration;
 using System.Data.Entity;
+using MvcSample.Domain.Users;
 
 namespace MvcSample.Core
 {
-    public class MvcSampleContext :DbContext
+    public class MvcSampleContext : DbContext
     {
-        public MvcSampleContext(): base(ConfigurationManager.ConnectionStrings["DefaultConnection"].Name)
+        #region "Constructors"
+
+        public MvcSampleContext() : base(ConfigurationManager.ConnectionStrings["DefaultConnection"].Name)
         {
-            
+
         }
+
+        #endregion
+
+        #region "Entities"
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
     }
 }
